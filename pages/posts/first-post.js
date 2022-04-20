292d120eb2914aeca1fb724f7968657aed7e1443
en0c-026 <bitup.games@gmail.com>
2022-04-20 11:17:07 +0200

    update first and home pages with layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,19 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Layuot from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
+    <Layuot>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href='/'>
           <a>Back home</a>
         </Link>
       </h2>
-    </>
+    </Layuot>
   )
 }
